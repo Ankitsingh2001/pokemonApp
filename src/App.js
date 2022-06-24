eb75ec00ebd3dd3856750cb812ba1fe39d16f569
Ankit Singh <ans.azm276122@gmail.com>
2022-06-24 07:33:53 +0530

    this is my fist react app to upload in github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  
+  const [pokemonName,setPokemonName] =useState("");
+  const [pokemonChosen , setPokemonChosen] = useState(false);
+  const [pokemon,setPokemon] = useState({
+          name:"" ,
+           species: "",
+           img:" ",
+           hp:"",
+           attack :"",
+           defense:"",
+           type:""
+        });
+  const searchPokemon =()=>{
+    (!pokemonName)?alert("Please enter valid pokemon"):(
+      Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) =>{
+         setPokemon({
+          name:pokemonName.toUpperCase() ,
+           species: response.data.species.name,
+           img:response.data.sprites.front_default,
+           hp:response.data.stats[0].base_stat,
+           attack :response.data.stats[1].base_stat,
+           defense:response.data.stats[2].base_stat,
+           type:response.data.types[0].type.name
+           });
+           setPokemonChosen(true);
+       }
+      )
+    )
+   }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='titleSection'> 
+        <h1>POKEMON STATS</h1>
+        <input 
+          type="text"
+          onChange={(event)=>{
+            setPokemonName(event.target.value.toLowerCase());
+          }}
+
+        />
+        <button onClick={searchPokemon}>Search pokemon</button>
+      </div>
+      <div className='displaySection'>
+         {!pokemonChosen ? (
+          <p>Please chose pokemon</p>
+         ):(
+          <>
+            <h2 >{pokemon.name}</h2>
+            <img src={pokemon.img} alt={pokemon.name}/>
+             <h2>Species: {pokemon.species}</h2> 
+            <h5>Type: {pokemon.type}</h5>
+            <h5>hp: {pokemon.hp}</h5>
+            <h5>Attack: {pokemon.attack}</h5>
+            <h5>Defence: {pokemon.defense}</h5>
+            
+          </>
+         )
+
+         }
+      </div>
     </div>
   );
 }
-
 export default App;
